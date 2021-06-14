6f32d2b8bf0b8ce290e816d366342d05c1db8c0c
Matthew Steeples <matthew@ledgerscope.com>
2021-06-14 21:19:44 +0100

    Include KeyStorage (for build)

---
+++ FreeAgent.Tests/KeyStorage.cs
@@ -0,0 +1,13 @@
+﻿namespace FreeAgent.Tests
+{
+    public class KeyStorage
+    {
+        public static string AppKey = "YOUR_API_KEY_HERE";
+        public static string AppSecret = "YOUR_API_SECRET_HERE";
+
+        public static string RefreshToken = "TOKEN TO USE FOR REFRESHING";
+
+        public static bool UseSandbox;
+        public static bool UseProxy;
+    }
+}
