9ca90f4157a262ce2494722ccdb7b432a0bf8d66
Tomislav Barbaric <tomislav@ledgerscope.com>
2021-09-30 22:00:06 +0200

    Created ExpenseClient.GetExpenseSorted (will be used for FreeAgentCompanyDataRetriever)

--- FreeAgent/Client/ExpenseClient.cs
+++ FreeAgent/Client/ExpenseClient.cs
@@ -25,6 +25,25 @@ namespace FreeAgent.Client
             return wrapper.expense;
         }
 
+        public enum Order { Earliest, Latest };
+
+        public List<Expense> GetExpenseSorted(Order order)
+        {
+            var request = CreateBasicRequest(Method.GET);
+
+            request.AddParameter("page", 1, ParameterType.GetOrPost);
+            request.AddParameter("per_page", 1, ParameterType.GetOrPost);
+
+            // To sort in descending order, the sort parameter can be prefixed with a hyphen
+            var sort = order == Order.Earliest ? "dated_on" : "-dated_on";
+
+            request.AddParameter("sort", sort, ParameterType.GetOrPost);
+
+            var response = Client.Execute<ExpensesWrapper>(request);
+
+            return response.expenses;
+        }
+
         public List<Expense> All(string view = "", string from_date = "", string to_date = "")
         {
             return All((request) =>
